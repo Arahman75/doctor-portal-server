f7ada235a2c046d62b6c93603166d35f44d2be0c
Arahmna75 <selfaraman01825@gmail.com>
2022-07-02 22:36:55 +0600

    open problem code

--- index.js
+++ index.js
@@ -25,26 +25,26 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 //if don't work this is on
 
-async function verifyToken(req, res, next) {
-  if (req.headers?.authorization?.startsWith('Bearer ')) {
-    const token = req.headers.authorization.split(' ')[1]
-  }
-  next();
-}
-
-// async function verifyToken(req, res, next){
-//   if(req.headers?.authorization?.startsWith('Bearer ')){
+// async function verifyToken(req, res, next) {
+//   if (req.headers?.authorization?.startsWith('Bearer ')) {
 //     const token = req.headers.authorization.split(' ')[1]
-//     try{
-// const decodedUser = await admin.auth().verifyIdToken(token);
-// req.decodedEmail = decodedUser.email;
-//     }
-//     catch{
-
-//     }
 //   }
 //   next();
 // }
+//new start
+async function verifyToken(req, res, next){
+  if(req.headers?.authorization?.startsWith('Bearer ')){
+    const token = req.headers.authorization.split(' ')[1]
+    try{
+const decodedUser = await admin.auth().verifyIdToken(token);
+req.decodedEmail = decodedUser.email;
+    }
+    catch{
+
+    }
+  }
+  next();
+}
 
 async function run() {
   try {
@@ -72,17 +72,17 @@ async function run() {
       const result = await appointmentsCollection.findOne(query);
       res.json(result)
     })
-
-    // app.get('/appointments',verifyToken, async(req, res) =>{
-    //   const email = req.query.email;
-    //   const date =new Date (req.query.date).toLocaleDateString();
-    //   // console.log(date);
-    //   const query = {email: email ,date: date};
-    //   // console.log(query);
-    //   const cursor = appointmentsCollection.find(query);
-    //   const appointments = await cursor.toArray();
-    //   res.json(appointments);
-    // });
+//new start
+    app.get('/appointments',verifyToken, async(req, res) =>{
+      const email = req.query.email;
+      const date =new Date (req.query.date).toLocaleDateString();
+      // console.log(date);
+      const query = {email: email ,date: date};
+      // console.log(query);
+      const cursor = appointmentsCollection.find(query);
+      const appointments = await cursor.toArray();
+      res.json(appointments);
+    });
 
     app.post('/appointments', async (req, res) => {
       const appointment = req.body;
@@ -136,32 +136,34 @@ async function run() {
 
     //the code don't work this is on admin verify token
 
-    app.put('/users/admin', verifyToken, async (req, res) => {
-      const user = req.body;
-      console.log('put', req.headers.authorization);
-      const filter = { email: user.email };
-      const updateDoc = { $set: { role: 'admin' } };
-      const result = await usersCollection.updateOne(filter, updateDoc);
-      res.json(result)
-    })
+    // app.put('/users/admin', verifyToken, async (req, res) => {
+    //   const user = req.body;
+    //   console.log('put', req.headers.authorization);
+    //   const filter = { email: user.email };
+    //   const updateDoc = { $set: { role: 'admin' } };
+    //   const result = await usersCollection.updateOne(filter, updateDoc);
+    //   res.json(result)
+    // })
 
-    //     app.put('/users/admin', verifyToken, async(req, res) =>{
-    //       const user = req.body;
-    //       const requester = req.decodedEmail;
-    // if(requester){
-    //   const requesterAccount = await usersCollection.findOne({email: requester});
-    //   if(requesterAccount.role === 'admin'){
-    //     const filter = {email: user.email};
-    //     const updateDoc = {$set: {role: 'admin'}};
-    //     const result = await usersCollection.updateOne(filter, updateDoc);
-    //     res.json(result)
-    //   }
-    // }
-    // else{
-    //   res.status(403).json({message: 'You do not have access make an admin'})
-    // }
 
-    //     })
+    //new start
+        app.put('/users/admin', verifyToken, async(req, res) =>{
+          const user = req.body;
+          const requester = req.decodedEmail;
+    if(requester){
+      const requesterAccount = await usersCollection.findOne({email: requester});
+      if(requesterAccount.role === 'admin'){
+        const filter = {email: user.email};
+        const updateDoc = {$set: {role: 'admin'}};
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.json(result)
+      }
+    }
+    else{
+      res.status(403).json({message: 'You do not have access make an admin'})
+    }
+
+        })
 
     //Payment api
     app.post('/create-payment-intent', async (req, res) => {
